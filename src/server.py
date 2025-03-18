f3f5e6e94fd73fdde2f4344fa5d50b544096cd28
Matt Poland <Matt.Poland@greenstonefcs.com>
2025-03-18 16:04:36 -0400

    Refine logging

--- src/server.py
+++ src/server.py
@@ -33,21 +33,24 @@ def require_api_key(f):
         if not hasattr(g, 'context'):
             g.context = {}
 
-        logging.info(f"Request headers: {request.headers}")
-
         mac_password = request.headers.get("x-mac-pw")
-       
         auth_header = request.headers.get("Authorization")
         requested_api_key = auth_header.split(" ")[1] if auth_header else None
+
         g.context = load_context_for_authenticated_user(requested_api_key, mac_password)  # Pass mac_password
-        
+
         if not g.context:
+            logging.warning("Unauthorized access attempt with invalid API key.")
             return jsonify({"error": "Unauthorized"}), 401
-        
+
         # Check if the API key is enabled
-        if not g.context.get("IS_ENABLED"):  # Updated to "IS_ENABLED"
+        if not g.context.get("IS_ENABLED"):
+            logging.warning(f"Access denied for disabled account: {g.context.get('USERNAME')}")
             return jsonify({"error": "Account not enabled"}), 403
-        
+
+        # Log the username for successful authentication
+        logging.info(f"Authenticated user: {g.context.get('USERNAME')}")
+
         return f(*args, **kwargs)
     return decorated_function
 
@@ -55,10 +58,17 @@ def require_admin(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'username' not in session:
+            logging.warning("Unauthorized admin access attempt: No username in session.")
             return jsonify({"error": "Unauthorized"}), 401
+
         auth_entry = get_auth(session['username'])
         if not auth_entry or not auth_entry.get("is_admin"):
+            logging.warning(f"Unauthorized admin access attempt by user: {session.get('username')}")
             return jsonify({"error": "Unauthorized"}), 401
+
+        # Log the username for successful admin authentication
+        logging.info(f"Admin access granted to user: {session['username']}")
+
         return f(*args, **kwargs)
     return decorated_function
 
